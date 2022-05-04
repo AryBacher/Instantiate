cc76bffefefa6718319a423018ed91f67d8b543f
AryBacher <47170979@est.ort.edu.ar>
2022-05-04 15:49:41 -0300

    Initial commit

---
+++ Assets/CloneObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour
+{
+    int num1 = 0;
+    public GameObject ObjectToClone;  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Instantiateobject()
+    {
+        while (num1 < 3)
+        {
+            Instantiate(ObjectToClone);
+            num1 += 1;
+        }
+
+        num1 = 0;
+    }
+}
